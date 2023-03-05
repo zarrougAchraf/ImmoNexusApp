fd9203997548cf70345e090b73501f8e25d5856a
Mahdi zgoulli <mahdi.zgolli@esprit.tn>
2023-03-05 18:05:27 +0100

    LastChanges

---
+++ src/main/java/tn/devteam/immonexus/Repository/BadWordsService.java
@@ -0,0 +1,53 @@
+package tn.devteam.immonexus.Repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tn.devteam.immonexus.Entities.BadWords;
+import tn.devteam.immonexus.Interfaces.IBadWordsService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BadWordsService implements IBadWordsService {
+    @Autowired
+    private BadWordsRepository badWordsRepository;
+
+    @Override
+    public BadWords addBadWord(BadWords badWords) {
+        return badWordsRepository.save(badWords);
+    }
+
+    @Override
+    // read
+    public List<BadWords> getAllBadWords() {
+        return badWordsRepository.findAll();
+    }
+
+    @Override
+    public Optional<BadWords> getBadWordById(Long id) {
+        return badWordsRepository.findById(id);
+    }
+
+   // @Override
+   // public BadWords getBadWordByWord(String word) {
+    //    return badWordsRepository.findByWord(word);
+   // }
+
+    @Override
+    public BadWords updateBadWord(Long id, BadWords badWords) {
+        Optional<BadWords> existingBadWord = badWordsRepository.findById(id);
+        if (existingBadWord.isPresent()) {
+            return badWordsRepository.save(badWords);
+        } else {
+            throw new RuntimeException("Bad word not found with id " + id);
+        }
+    }
+
+    @Override
+    // delete
+    public void deleteBadWord(Long id) {
+        badWordsRepository.deleteById(id);
+    }
+
+}
